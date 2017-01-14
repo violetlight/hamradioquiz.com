f921a738c180d716afdc25b13f954114435e45aa
JF <jasonhfields@gmail.com>
2017-01-13 21:51:44 -0600

    Removed this db query from the index route

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,8 @@ var Question = require('../models/question');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var ctx = { title: 'Express' };
-  Question.find({}, function(err, questions) {
-    ctx['questions'] = questions;
-    ctx['user'] = req.user;
-    res.render('index', ctx);
-  });
+  ctx['user'] = req.user;
+  res.render('index', ctx);
 });
 
 module.exports = router;
