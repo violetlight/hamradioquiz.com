27443b7b81d8fe9df5d67b3fa7406ff404c127a0
JF <jasonhfields@gmail.com>
2017-01-16 01:56:39 -0600

    AJAX POST to check answer

--- public/javascripts/quizForm.js
+++ public/javascripts/quizForm.js
@@ -1,8 +1,21 @@
 var form = $('form');
 
-
 form.on('submit', function(e) {
-  var formData = form.serialize();
   e.preventDefault();
-  console.log(formData);
+
+  var formData = {};
+  $.each(form.serializeArray(), function(i, formInput) {
+    formData[formInput['name']] = formInput['value'];
+  });
+
+  $.ajax({
+    method: 'POST',
+    url: '/quiz/checkAnswer',
+    data: formData
+  }).done(function(response, status) {
+    // response should be whether or not the answer was correct
+    // update DOM accordingly
+    console.log(response);
+  });
 });
+
